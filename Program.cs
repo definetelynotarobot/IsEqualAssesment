0aea53ce6f8ea2fa7b039e6d179106270fb8b960
Mehmet Emin Eraslan <mehmetemineraslan7@gmail.com>
2024-03-07 21:06:21 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,56 @@
+﻿var firstStrings = new List<string> { "aaa", "abbc", "zyz" };
+var secondStrings = new List<string> { "bbb", "cccc", "zyx" };
+var res = canBeEqualized(firstStrings, secondStrings);
+
+
+foreach (var item in res)
+{
+    Console.WriteLine(item);
+}
+
+
+List<string> canBeEqualized(List<string> firstStrings, List<string> secondStrings)
+{
+    var result = new List<string>();
+    for (int i = 0; i < firstStrings.Count; i++)
+    {
+        string s1 = firstStrings[i];
+        string s2 = secondStrings[i];
+        if (s1.Length != s2.Length)
+        {
+            result.Add("NO");
+            continue;
+        }
+        if (s1 == s2)
+            result.Add("YES");
+        {
+            for (int j = 0; j < s1.Length; j++)
+            {
+                if (s1[j] != s2[j])
+                {
+                    var temp = s2.Replace(s2[j], s1[j]);
+                    if (s1 == temp)
+                        result.Add("YES");
+                        
+
+                    temp = s1.Replace(s1[j], s2[j]);
+                    if (s2 == temp)
+                        result.Add("NO");
+                        
+                }
+
+            }
+            //Ekstra NO eklediği ve gerek duyulmadığı için kaldırdım.
+            result.Add("NO");
+
+        }
+    
+    }
+    return result;
+}
+
+/*
+string isBeEqualized(string s1, string s2)
+{
+    
+}*/
